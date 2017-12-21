913dc3509b10c800a27c99a1329503b2b5f54cdc
mghasemi <mehdi.ghasemi@gmail.com>
2017-12-21 17:06:47 -0600

    Initial commit.

--- Optim/base.py
+++ Optim/base.py
@@ -1,20 +1,47 @@
+from __future__ import print_function
+
+
 class OptimTemplate(object):
     def __init__(self):
         self.MaxIteration = 100
         self.ErrorTolerance = 1e-6
+        self.STEP = 0
+        self.Success = False
         self.Terminate = False
+        self.objective = None
+        self.constraints = None
+        self.iteration_message = None
+        self.termination_message = None
 
-    def iterate(self):
-        pass
+    def iterate(self, **kwargs):
+        self.STEP += 1
 
-    def terminate(self):
+    def terminate(self, **kwargs):
+        if self.STEP >= self.MaxIteration:
+            self.Terminate = True
+            self.termination_message = "Maximum iteration reached."
         self.Terminate = True
-        return True
+        return self.Terminate
 
 
 class Base(object):
-    def __init__(self):
+    def __init__(self, **kwargs):
         self.x0 = None
         self.objective = lambda x: 0
         self.constraints = []
-        self.STEP = 0
+        self.Verbose = True
+        _optimizer = kwargs.pop('method', OptimTemplate)
+        self.optimizer = _optimizer(**kwargs)
+
+    def __call__(self, *args, **kwargs):
+        # Iterate:
+        while not self.optimizer.terminate(**kwargs):
+            self.optimizer.iterate(**kwargs)
+            # Prompt the iteration message:
+            if self.Verbose:
+                if self.optimizer.iteration_message is not None:
+                    print(self.optimizer.iteration_message)
+        # Prompt termination message:
+        if self.Verbose:
+            if self.optimizer.termination_message is not None:
+                print(self.optimizer.termination_message)
